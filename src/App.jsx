4dbf2966758ec06851e3a14b8d2c89220252837f
JavierLRA-Jimenez <javierjimenezmk@gmail.com>
2023-12-03 23:25:31 -0300

    publicacion 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Monsters from './Components/Monsters'
+import Hero from './Components/hero'
+
+const App = () => {
+  return (
+    <section>
+      <Hero/>
+      <Monsters/>
+    </section>
+  )
+}
+
+export default App
